28232d598dba83ea8901ab36918b22f21627cf1f
testtimshaq <58642417+testtimshaq@users.noreply.github.com>
2021-01-14 08:49:49 +0500

    week-thing & fashion-ideas page

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,15 @@
 "use strict";
 
 import saleCard from './saleCard.js';
+import weekThingPopups from './weekThingPopups.js';
 
 document.addEventListener("DOMContentLoaded", function () {
 
 	var header = document.getElementById('header');
 
 	saleCard();
+
+	weekThingPopups();
 
 
 });
---
+++ src/js/weekThingPopups.js
@@ -0,0 +1,77 @@
+export default function() {
+
+
+
+	const thingsNL = document.querySelectorAll('li.week-thing__thing.thing');
+
+	if (!thingsNL.length) return;
+
+	const thingsList = Array.prototype.slice.call(thingsNL);
+
+
+
+
+	const html = document.getElementById('html')
+
+	function disableScroll() {
+		html.style.overflowX = 'hidden';
+		html.style.overflowY = 'hidden';
+	};
+
+	function enableScroll() {
+		html.style.overflowX = 'hidden';
+		html.style.overflowY = 'scroll';
+	};
+
+	function closeEl(el) {
+		el.classList.remove('open');
+		enableScroll();
+	}
+
+	function openEl(el) {
+		el.classList.add('open');
+		disableScroll();
+	}
+
+
+
+
+	const popup = document.querySelector('.popup-cont');
+	const popupClose = popup.querySelector('.popup__close');
+	const popupImg = popup.querySelector('.popup__ph');
+	const popupText = popup.querySelector('.popup__article');
+
+	thingsList.forEach(thing => {
+
+		thing.addEventListener('click', function() {
+
+			if(popup.classList.contains('open')) {
+				closeEl(popup);
+			} else {
+				const thingImg = thing.querySelector('.thing__ph').getAttribute('src');
+				popupImg.setAttribute('src', thingImg);
+
+				var textForPopup = thing.querySelector('.thing__popup-text').innerText;
+
+				console.log(textForPopup)
+
+				if(!textForPopup) textForPopup = 'Описание не найдено';
+
+				popupText.innerText = textForPopup;
+
+				openEl(popup);
+			}
+
+		})
+
+	})
+
+	popupClose.addEventListener('click', function() {
+		closeEl(popup);
+	})
+
+	popup.addEventListener('click', function(ev) {
+		if(ev.target === this) closeEl(popup);
+	})
+
+}
