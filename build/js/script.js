c66ca0bef0c92efe064f8f9053f7bb8c04837556
testtimshaq <58642417+testtimshaq@users.noreply.github.com>
2021-01-20 11:03:19 +0500

    step5 & footer

--- build/js/script.js
+++ build/js/script.js
@@ -2,140 +2,353 @@
 
 function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
-!function (e) {
-  var t = {};
+/******/
+(function (modules) {
+  // webpackBootstrap
 
-  function n(o) {
-    if (t[o]) return t[o].exports;
-    var r = t[o] = {
-      i: o,
-      l: !1,
+  /******/
+  // The module cache
+
+  /******/
+  var installedModules = {};
+  /******/
+
+  /******/
+  // The require function
+
+  /******/
+
+  function __webpack_require__(moduleId) {
+    /******/
+
+    /******/
+    // Check if module is in cache
+
+    /******/
+    if (installedModules[moduleId]) {
+      /******/
+      return installedModules[moduleId].exports;
+      /******/
+    }
+    /******/
+    // Create a new module (and put it into the cache)
+
+    /******/
+
+
+    var module = installedModules[moduleId] = {
+      /******/
+      i: moduleId,
+
+      /******/
+      l: false,
+
+      /******/
       exports: {}
+      /******/
+
     };
-    return e[o].call(r.exports, r, r.exports, n), r.l = !0, r.exports;
+    /******/
+
+    /******/
+    // Execute the module function
+
+    /******/
+
+    modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+    /******/
+
+    /******/
+    // Flag the module as loaded
+
+    /******/
+
+    module.l = true;
+    /******/
+
+    /******/
+    // Return the exports of the module
+
+    /******/
+
+    return module.exports;
+    /******/
   }
+  /******/
 
-  n.m = e, n.c = t, n.d = function (e, t, o) {
-    n.o(e, t) || Object.defineProperty(e, t, {
-      enumerable: !0,
-      get: o
-    });
-  }, n.r = function (e) {
-    "undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, {
-      value: "Module"
-    }), Object.defineProperty(e, "__esModule", {
-      value: !0
-    });
-  }, n.t = function (e, t) {
-    if (1 & t && (e = n(e)), 8 & t) return e;
-    if (4 & t && "object" == _typeof(e) && e && e.__esModule) return e;
-    var o = Object.create(null);
-    if (n.r(o), Object.defineProperty(o, "default", {
-      enumerable: !0,
-      value: e
-    }), 2 & t && "string" != typeof e) for (var r in e) {
-      n.d(o, r, function (t) {
-        return e[t];
-      }.bind(null, r));
+  /******/
+
+  /******/
+  // expose the modules object (__webpack_modules__)
+
+  /******/
+
+
+  __webpack_require__.m = modules;
+  /******/
+
+  /******/
+  // expose the module cache
+
+  /******/
+
+  __webpack_require__.c = installedModules;
+  /******/
+
+  /******/
+  // define getter function for harmony exports
+
+  /******/
+
+  __webpack_require__.d = function (exports, name, getter) {
+    /******/
+    if (!__webpack_require__.o(exports, name)) {
+      /******/
+      Object.defineProperty(exports, name, {
+        enumerable: true,
+        get: getter
+      });
+      /******/
     }
-    return o;
-  }, n.n = function (e) {
-    var t = e && e.__esModule ? function () {
-      return e["default"];
-    } : function () {
-      return e;
+    /******/
+
+  };
+  /******/
+
+  /******/
+  // define __esModule on exports
+
+  /******/
+
+
+  __webpack_require__.r = function (exports) {
+    /******/
+    if (typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+      /******/
+      Object.defineProperty(exports, Symbol.toStringTag, {
+        value: 'Module'
+      });
+      /******/
+    }
+    /******/
+
+
+    Object.defineProperty(exports, '__esModule', {
+      value: true
+    });
+    /******/
+  };
+  /******/
+
+  /******/
+  // create a fake namespace object
+
+  /******/
+  // mode & 1: value is a module id, require it
+
+  /******/
+  // mode & 2: merge all properties of value into the ns
+
+  /******/
+  // mode & 4: return value when already ns object
+
+  /******/
+  // mode & 8|1: behave like require
+
+  /******/
+
+
+  __webpack_require__.t = function (value, mode) {
+    /******/
+    if (mode & 1) value = __webpack_require__(value);
+    /******/
+
+    if (mode & 8) return value;
+    /******/
+
+    if (mode & 4 && _typeof(value) === 'object' && value && value.__esModule) return value;
+    /******/
+
+    var ns = Object.create(null);
+    /******/
+
+    __webpack_require__.r(ns);
+    /******/
+
+
+    Object.defineProperty(ns, 'default', {
+      enumerable: true,
+      value: value
+    });
+    /******/
+
+    if (mode & 2 && typeof value != 'string') for (var key in value) {
+      __webpack_require__.d(ns, key, function (key) {
+        return value[key];
+      }.bind(null, key));
+    }
+    /******/
+
+    return ns;
+    /******/
+  };
+  /******/
+
+  /******/
+  // getDefaultExport function for compatibility with non-harmony modules
+
+  /******/
+
+
+  __webpack_require__.n = function (module) {
+    /******/
+    var getter = module && module.__esModule ?
+    /******/
+    function getDefault() {
+      return module['default'];
+    } :
+    /******/
+    function getModuleExports() {
+      return module;
     };
-    return n.d(t, "a", t), t;
-  }, n.o = function (e, t) {
-    return Object.prototype.hasOwnProperty.call(e, t);
-  }, n.p = "", n(n.s = 4);
-}([function (e, t, n) {
-  "use strict";
+    /******/
 
-  n.r(t), t["default"] = function () {
-    var e = document.getElementById("btnBurger");
-    if (!e) return;
-    var t = document.getElementById("menuTooltip");
-    e.addEventListener("click", function () {
-      t.classList.contains("open") ? t.classList.remove("open") : (t.classList.add("open"), t.focus(), t.onblur = function (n) {
-        n.relatedTarget !== e && t.classList.remove("open");
-      });
-    });
+    __webpack_require__.d(getter, 'a', getter);
+    /******/
+
+
+    return getter;
+    /******/
   };
-}, function (e, t, n) {
-  "use strict";
+  /******/
 
-  n.r(t), t["default"] = function () {
-    var e = document.querySelectorAll(".sale-card");
-    if (!e.length) return;
-    Array.prototype.slice.call(e).forEach(function (e) {
-      var t = e.querySelector("button.sale-card__link");
-      t.addEventListener("click", function () {
-        e.classList.contains("open") ? (e.classList.remove("open"), t.innerText = "Подробнее >>") : (e.classList.add("open"), t.innerText = "Скрыть >>");
-      });
-    });
+  /******/
+  // Object.prototype.hasOwnProperty.call
+
+  /******/
+
+
+  __webpack_require__.o = function (object, property) {
+    return Object.prototype.hasOwnProperty.call(object, property);
   };
-}, function (e, t, n) {
-  "use strict";
+  /******/
 
-  n.r(t), t["default"] = function () {
-    var e = document.querySelectorAll("li.week-thing__thing.thing");
-    if (!e.length) return;
-    var t = Array.prototype.slice.call(e),
-        n = document.getElementById("html");
+  /******/
+  // __webpack_public_path__
 
-    function o(e) {
-      e.classList.remove("open"), n.style.overflowX = "hidden", n.style.overflowY = "scroll";
-    }
+  /******/
 
-    function r(e) {
-      e.classList.add("open"), n.style.overflowX = "hidden", n.style.overflowY = "hidden";
-    }
 
-    var c = document.querySelector(".popup-cont"),
-        i = c.querySelector(".popup__close"),
-        l = c.querySelector(".popup__ph"),
-        u = c.querySelector(".popup__article");
-    t.forEach(function (e) {
-      e.addEventListener("click", function () {
-        if (c.classList.contains("open")) o(c);else {
-          var _n = e.querySelector(".thing__ph").getAttribute("src");
+  __webpack_require__.p = "";
+  /******/
 
-          l.setAttribute("src", _n);
-          var t = e.querySelector(".thing__popup-text").innerText;
-          console.log(t), t || (t = "Описание не найдено"), u.innerText = t, r(c);
-        }
-      });
-    }), i.addEventListener("click", function () {
-      o(c);
-    }), c.addEventListener("click", function (e) {
-      e.target === this && o(c);
-    });
-  };
-}, function (e, t, n) {
-  "use strict";
+  /******/
 
-  n.r(t), t["default"] = function () {
-    var e = document.querySelectorAll(".service-faq .service-faq__li");
-    if (!e.length) return;
-    Array.prototype.slice.call(e).forEach(function (e) {
-      e.addEventListener("click", function () {
-        e.classList.contains("open") ? e.classList.remove("open") : e.classList.add("open");
-      });
-    });
-  };
-}, function (e, t, n) {
-  n(0), n(1), n(5), n(3), e.exports = n(2);
-}, function (e, t, n) {
-  "use strict";
+  /******/
+  // Load entry module and return exports
 
-  n.r(t);
-  var o = n(1),
-      r = n(0),
-      c = n(2),
-      i = n(3);
-  document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("header");
-    Object(r["default"])(), Object(o["default"])(), Object(c["default"])(), Object(i["default"])();
-  });
-}]);
+  /******/
+
+  return __webpack_require__(__webpack_require__.s = 0);
+  /******/
+})(
+/************************************************************************/
+
+/******/
+{
+  /***/
+  "./src/js/btnBurger.js":
+  /*!*****************************!*\
+    !*** ./src/js/btnBurger.js ***!
+    \*****************************/
+
+  /*! exports provided: default */
+
+  /***/
+  function srcJsBtnBurgerJs(module, __webpack_exports__, __webpack_require__) {
+    "use strict";
+
+    eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (function () {\r\n\r\n\tconst btnBurger = document.getElementById('btnBurger');\r\n\r\n\tif(!btnBurger) return;\r\n\r\n\tconst menuTooltip = document.getElementById('menuTooltip');\r\n\r\n\tbtnBurger.addEventListener('click', function() {\r\n\r\n\t\tif(menuTooltip.classList.contains('open')) {\r\n\t\t\tmenuTooltip.classList.remove('open');\r\n\t\t} else {\r\n\t\t\tmenuTooltip.classList.add('open');\r\n\t\t\tmenuTooltip.focus();\r\n\t\t\tmenuTooltip.onblur = function(ev) {\r\n\t\t\t\tif(ev.relatedTarget === btnBurger) return;\r\n\t\t\t\tmenuTooltip.classList.remove('open');\r\n\t\t\t};\r\n\t\t}\r\n\r\n\t})\r\n\r\n\r\n});\n\n//# sourceURL=webpack:///./src/js/btnBurger.js?");
+    /***/
+  },
+
+  /***/
+  "./src/js/saleCard.js":
+  /*!****************************!*\
+    !*** ./src/js/saleCard.js ***!
+    \****************************/
+
+  /*! exports provided: default */
+
+  /***/
+  function srcJsSaleCardJs(module, __webpack_exports__, __webpack_require__) {
+    "use strict";
+
+    eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (function () {\r\n\r\n\tconst cardsNL = document.querySelectorAll('.sale-card');\r\n\r\n\tif (!cardsNL.length) return;\r\n\r\n\r\n\tconst cardsList = Array.prototype.slice.call(cardsNL);\r\n\r\n\tcardsList.forEach(card => {\r\n\r\n\t\tconst btn = card.querySelector('button.sale-card__link');\r\n\r\n\t\tbtn.addEventListener('click', function() {\r\n\t\t\tif(card.classList.contains('open')) {\r\n\t\t\t\tcard.classList.remove('open');\r\n\t\t\t\tbtn.innerText = 'Подробнее >>';\r\n\t\t\t} else {\r\n\t\t\t\tcard.classList.add('open');\r\n\t\t\t\tbtn.innerText = 'Скрыть >>'\r\n\t\t\t}\r\n\t\t})\r\n\r\n\t})\r\n});\n\n//# sourceURL=webpack:///./src/js/saleCard.js?");
+    /***/
+  },
+
+  /***/
+  "./src/js/script.js":
+  /*!**************************!*\
+    !*** ./src/js/script.js ***!
+    \**************************/
+
+  /*! no exports provided */
+
+  /***/
+  function srcJsScriptJs(module, __webpack_exports__, __webpack_require__) {
+    "use strict";
+
+    eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _saleCard_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./saleCard.js */ \"./src/js/saleCard.js\");\n/* harmony import */ var _btnBurger_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./btnBurger.js */ \"./src/js/btnBurger.js\");\n/* harmony import */ var _weekThingPopups_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./weekThingPopups.js */ \"./src/js/weekThingPopups.js\");\n/* harmony import */ var _serviceFaq_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./serviceFaq.js */ \"./src/js/serviceFaq.js\");\n\r\n\r\n\r\n\r\n\r\n\r\n\r\ndocument.addEventListener(\"DOMContentLoaded\", function () {\r\n\r\n\tvar header = document.getElementById('header');\r\n\r\n\tObject(_btnBurger_js__WEBPACK_IMPORTED_MODULE_1__[\"default\"])();\r\n\t\r\n\tObject(_saleCard_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"])();\r\n\r\n\tObject(_weekThingPopups_js__WEBPACK_IMPORTED_MODULE_2__[\"default\"])();\r\n\r\n\tObject(_serviceFaq_js__WEBPACK_IMPORTED_MODULE_3__[\"default\"])();\r\n\r\n\r\n});\n\n//# sourceURL=webpack:///./src/js/script.js?");
+    /***/
+  },
+
+  /***/
+  "./src/js/serviceFaq.js":
+  /*!******************************!*\
+    !*** ./src/js/serviceFaq.js ***!
+    \******************************/
+
+  /*! exports provided: default */
+
+  /***/
+  function srcJsServiceFaqJs(module, __webpack_exports__, __webpack_require__) {
+    "use strict";
+
+    eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (function () {\r\n\r\n\tconst serviceFaqLiNL = document.querySelectorAll('.service-faq .service-faq__li');\r\n\r\n\tif (!serviceFaqLiNL.length) return;\r\n\r\n\r\n\tconst serviceFaqLiList = Array.prototype.slice.call(serviceFaqLiNL);\r\n\r\n\tserviceFaqLiList.forEach(li => {\r\n\t\tli.addEventListener('click', function() {\r\n\t\t\tif(li.classList.contains('open')) {\r\n\t\t\t\tli.classList.remove('open');\r\n\t\t\t} else {\r\n\t\t\t\tli.classList.add('open');\r\n\t\t\t}\r\n\t\t})\r\n\t})\r\n\r\n});\n\n//# sourceURL=webpack:///./src/js/serviceFaq.js?");
+    /***/
+  },
+
+  /***/
+  "./src/js/weekThingPopups.js":
+  /*!***********************************!*\
+    !*** ./src/js/weekThingPopups.js ***!
+    \***********************************/
+
+  /*! exports provided: default */
+
+  /***/
+  function srcJsWeekThingPopupsJs(module, __webpack_exports__, __webpack_require__) {
+    "use strict";
+
+    eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (function() {\r\n\r\n\r\n\r\n\tconst thingsNL = document.querySelectorAll('li.week-thing__thing.thing');\r\n\r\n\tif (!thingsNL.length) return;\r\n\r\n\tconst thingsList = Array.prototype.slice.call(thingsNL);\r\n\r\n\r\n\r\n\r\n\tconst html = document.getElementById('html')\r\n\r\n\tfunction disableScroll() {\r\n\t\thtml.style.overflowX = 'hidden';\r\n\t\thtml.style.overflowY = 'hidden';\r\n\t};\r\n\r\n\tfunction enableScroll() {\r\n\t\thtml.style.overflowX = 'hidden';\r\n\t\thtml.style.overflowY = 'scroll';\r\n\t};\r\n\r\n\tfunction closeEl(el) {\r\n\t\tel.classList.remove('open');\r\n\t\tenableScroll();\r\n\t}\r\n\r\n\tfunction openEl(el) {\r\n\t\tel.classList.add('open');\r\n\t\tdisableScroll();\r\n\t}\r\n\r\n\r\n\r\n\r\n\tconst popup = document.querySelector('.popup-cont');\r\n\tconst popupClose = popup.querySelector('.popup__close');\r\n\tconst popupImg = popup.querySelector('.popup__ph');\r\n\tconst popupText = popup.querySelector('.popup__article');\r\n\r\n\tthingsList.forEach(thing => {\r\n\r\n\t\tthing.addEventListener('click', function() {\r\n\r\n\t\t\tif(popup.classList.contains('open')) {\r\n\t\t\t\tcloseEl(popup);\r\n\t\t\t} else {\r\n\t\t\t\tconst thingImg = thing.querySelector('.thing__ph').getAttribute('src');\r\n\t\t\t\tpopupImg.setAttribute('src', thingImg);\r\n\r\n\t\t\t\tvar textForPopup = thing.querySelector('.thing__popup-text').innerText;\r\n\r\n\t\t\t\tconsole.log(textForPopup)\r\n\r\n\t\t\t\tif(!textForPopup) textForPopup = 'Описание не найдено';\r\n\r\n\t\t\t\tpopupText.innerText = textForPopup;\r\n\r\n\t\t\t\topenEl(popup);\r\n\t\t\t}\r\n\r\n\t\t})\r\n\r\n\t})\r\n\r\n\tpopupClose.addEventListener('click', function() {\r\n\t\tcloseEl(popup);\r\n\t})\r\n\r\n\tpopup.addEventListener('click', function(ev) {\r\n\t\tif(ev.target === this) closeEl(popup);\r\n\t})\r\n\r\n});\n\n//# sourceURL=webpack:///./src/js/weekThingPopups.js?");
+    /***/
+  },
+
+  /***/
+  0:
+  /*!******************************************************************************************************************************!*\
+    !*** multi ./src/js/btnBurger.js ./src/js/saleCard.js ./src/js/script.js ./src/js/serviceFaq.js ./src/js/weekThingPopups.js ***!
+    \******************************************************************************************************************************/
+
+  /*! no static exports found */
+
+  /***/
+  function _(module, exports, __webpack_require__) {
+    eval("__webpack_require__(/*! C:\\Users\\timur\\Desktop\\catalog-club\\src\\js\\btnBurger.js */\"./src/js/btnBurger.js\");\n__webpack_require__(/*! C:\\Users\\timur\\Desktop\\catalog-club\\src\\js\\saleCard.js */\"./src/js/saleCard.js\");\n__webpack_require__(/*! C:\\Users\\timur\\Desktop\\catalog-club\\src\\js\\script.js */\"./src/js/script.js\");\n__webpack_require__(/*! C:\\Users\\timur\\Desktop\\catalog-club\\src\\js\\serviceFaq.js */\"./src/js/serviceFaq.js\");\nmodule.exports = __webpack_require__(/*! C:\\Users\\timur\\Desktop\\catalog-club\\src\\js\\weekThingPopups.js */\"./src/js/weekThingPopups.js\");\n\n\n//# sourceURL=webpack:///multi_./src/js/btnBurger.js_./src/js/saleCard.js_./src/js/script.js_./src/js/serviceFaq.js_./src/js/weekThingPopups.js?");
+    /***/
+  }
+  /******/
+
+});
