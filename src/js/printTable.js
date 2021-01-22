79c482a96a49784091da3892c654055f5b73eb78
testtimshaq <58642417+testtimshaq@users.noreply.github.com>
2021-01-22 17:22:47 +0500

    pre-check

--- src/js/printTable.js
+++ src/js/printTable.js
@@ -7,19 +7,6 @@ if(!printBtn) return;
 const tableForPrint = document.getElementById('tableForPrint');
 
 
-printBtn.addEventListener('click', CallPrint(tableForPrint))
-
-function CallPrint(el) {
-	var prtContent = el;
-	var WinPrint = window.open('','','left=50,top=50,width=800,height=640,toolbar=0,scrollbars=1,status=0');
-	WinPrint.document.write('');
-	WinPrint.document.write(prtContent.innerHTML);
-	WinPrint.document.write('');
-	WinPrint.document.close();
-	WinPrint.focus();
-	WinPrint.print();
-	WinPrint.close();
-	prtContent.innerHTML=strOldOne;
-}
+printBtn.addEventListener('click', () => window.print())
 
 }
